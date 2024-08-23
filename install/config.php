736f6b754facecf214ce10b2a45045e1cede35d4
Michael Newton <mlnnflorida@gmail.com>
2024-08-23 16:53:50 -0400

    Update config.php

--- install/config.php
+++ install/config.php
@@ -17,7 +17,7 @@ $aConfig = array(
 	'name' => 'bx_messenger',
 	'title' => 'Messenger',
 	'note' => 'Messenger module.',
-	'version' => '13.0.5.DEV',
+	'version' => '13.0.5',
 	'vendor' => 'BoonEx',
 	'help_url' => 'http://feed.una.io/?section={module_name}',
 
